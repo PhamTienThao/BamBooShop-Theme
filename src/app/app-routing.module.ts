915e18872ecbda0e0f515d0f533bd364b7d1ab42
BamBoo <phamtienthao88@gmail.com>
2022-09-08 19:11:48 +0700

    Add-theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,128 @@
+import { ThankYouPageComponent } from './containers/client/thank-you-page/thank-you-page.component';
+import { EmptyCardComponent } from './containers/client/empty-card/empty-card.component';
+import { ArticleCategoryComponent } from './containers/client/article-category/article-category.component';
+import { LoginComponent } from './containers/client/login/login.component';
+import { ProfileComponent } from './containers/client/profile/profile.component';
+import { CartComponent } from './containers/client/cart/cart.component';
+import { CategoryComponent } from './containers/client/category/category.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductTemplateComponent } from './components/product-template/product-template.component';
+import { HomeComponent } from './containers/client/home/home.component';
+import { LayoutComponent } from './containers/client/layout/layout.component';
+import { PageNotFoundComponent } from './containers/client/page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './containers/client/product-detail/product-detail.component';
+import { ArticleComponent } from './containers/client/article/article.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+//   {
+// //   path: "admin",
+// //   loadChildren: () => import('./containers/admin/admin.module').then(m => m.AdminModule),
+// // },
+{
+  path: "",
+  component: LayoutComponent,
+  children: [
+//     {
+//       path: "profile",
+//       canActivate: [AuthGuardService],
+//       component: ProfileComponent
+//     },
+//     {
+//       path: "dang-nhap",
+//       component: LoginComponent
+//     },
+//     {
+//       path: "dang-ky-tai-khoan",
+//       component: SignUpComponent
+//     },
+//     {
+//       path: "dang-xuat",
+//       component: LogoutComponent
+//     },
+//     {
+//       path: "quen-mat-khau",
+//       component: ForgotPasswordComponent
+//     },
+//     {
+//       path: "gio-hang",
+//       canActivate: [AuthGuardService],
+//       component: CartComponent
+//     },
+//     {
+//       path: "dat-hang-thanh-cong",
+//       canActivate: [AuthGuardService],
+//       component: OrderSuccessfulComponent
+//     },
+//     {
+//       path: "danh-muc-bai-viet/:alias",
+//       component: ArticleCategoryComponent
+//     },
+//     {
+//       path: "bai-viet/:alias",
+//       component: ArticleComponent
+//     },
+//     {
+//       path: "thong-tin/:alias",
+//       component: ArticleExtComponent
+//     },
+    {
+      path: "san-pham", //:alias
+      component: ProductDetailComponent
+    },
+    {
+      path: "not-found",
+      component: PageNotFoundComponent
+    },
+    {
+      path: "dien-thoai",
+      component: CategoryComponent
+    },
+    {
+      path: "gio-hang",
+      component: CartComponent
+    },
+    {
+      path: "tai-khoan",
+      component: ProfileComponent
+    },
+    {
+      path: "dang-nhap",
+      component: LoginComponent
+    },
+    {
+      path: "danh-sach-bai-viet",
+      component: ArticleCategoryComponent
+    },
+    {
+      path: "bai-viet",
+      component: ArticleComponent
+    },
+    {
+      path: "gio-hang-trong",
+      component: EmptyCardComponent
+    },
+    {
+      path: "dat-hang-thanh-cong",
+      component: ThankYouPageComponent
+    },
+
+
+//     {
+//       path: "tim-kiem/:alias",
+//       component: SearchComponent
+//     },
+//     {
+//       path: ":alias",
+//       component: CategoryComponent
+//     },
+    {
+      path: "",
+      component: HomeComponent
+    }
+  ]
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,48 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductTemplateComponent } from './components/product-template/product-template.component';
+import { ArticleComponent } from './containers/client/article/article.component';
+import { ArticleCategoryComponent } from './containers/client/article-category/article-category.component';
+import { AuthComponent } from './containers/client/auth/auth.component';
+import { CartComponent } from './containers/client/cart/cart.component';
+import { CategoryComponent } from './containers/client/category/category.component';
+import { ForgotPasswordComponent } from './containers/client/forgot-password/forgot-password.component';
+import { HomeComponent } from './containers/client/home/home.component';
+import { LayoutComponent } from './containers/client/layout/layout.component';
+import { LoginComponent } from './containers/client/login/login.component';
+import { LogoutComponent } from './containers/client/logout/logout.component';
+import { OrderSuccessfulComponent } from './containers/client/order-successful/order-successful.component';
+import { PageNotFoundComponent } from './containers/client/page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './containers/client/product-detail/product-detail.component';
+import { ProfileComponent } from './containers/client/profile/profile.component';
+import { SearchComponent } from './containers/client/search/search.component';
+import { SignUpComponent } from './containers/client/sign-up/sign-up.component';
+import { EmptyCardComponent } from './containers/client/empty-card/empty-card.component';
+import { ThankYouPageComponent } from './containers/client/thank-you-page/thank-you-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductTemplateComponent,
+    ArticleComponent,
+    ArticleCategoryComponent,
+    AuthComponent,
+    CartComponent,
+    CategoryComponent,
+    ForgotPasswordComponent,
+    HomeComponent,
+    LayoutComponent,
+    LoginComponent,
+    LogoutComponent,
+    OrderSuccessfulComponent,
+    PageNotFoundComponent,
+    ProductDetailComponent,
+    ProfileComponent,
+    SearchComponent,
+    SignUpComponent,
+    EmptyCardComponent,
+    ThankYouPageComponent
   ],
   imports: [
     BrowserModule,
