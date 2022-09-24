01a680e93842f7e2e7a20182220b916eee9f3902
BamBoo <phamtienthao88@gmail.com>
2022-09-25 01:40:33 +0700

    login-forgotpassword-theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -40,7 +40,7 @@ const routes: Routes = [
         component: LoginComponent
       },
       {
-        path: "dang-ky-tai-khoan",
+        path: "dang-ky",
         component: SignUpComponent
       },
       {
--- src/app/containers/client/forgot-password/forgot-password.component.ts
+++ src/app/containers/client/forgot-password/forgot-password.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { finalize } from 'rxjs/operators';
+import { CustomerService } from 'src/app/core/service/customer.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +11,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
+  formData!: FormGroup;
+  nzLoading: boolean = false;
 
-  constructor() { }
+  constructor(
+    private customerService: CustomerService,
+    private messageService: NzMessageService,
+    private formBuilder: FormBuilder,
+    private ngZone: NgZone,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.formData = this.formBuilder.group({
+      Email: [null, [Validators.email, Validators.required]]
+    });
   }
 
+  onSubmit(): void {
+    for (const i in this.formData.controls) {
+      if (this.formData.controls.hasOwnProperty(i)) {
+        this.formData.controls[i].markAsDirty();
+        this.formData.controls[i].updateValueAndValidity();
+      }
+    }
+    if (this.formData.invalid) {
+      return;
+    }
+
+    this.nzLoading = true;
+    this.customerService.forgotPassword(this.formData.getRawValue().Email)
+      .pipe(
+        finalize(() => {
+          this.nzLoading = false;
+        })
+      )
+      .subscribe((resp: any) => {
+        this.messageService.success("Yêu cầu cấp lại mật khẩu thành công. Vui lòng kiểm tra thông tin tại hòm thư của bạn.");
+      }, (error: any) => {
+        this.messageService.error(error.error.message);
+      })
+  }
+
+  navigate(path: string): void {
+    this.ngZone.run(() => this.router.navigateByUrl(path)).then();
+  }
 }
+
+
